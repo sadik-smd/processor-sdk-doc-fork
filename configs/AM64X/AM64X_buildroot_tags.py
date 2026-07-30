3a8876cfac943dfdb3d18aaf337abd4695bc21aa
Mahammed Sadik Shaik <s-sadik@ti.com>
2026-07-30 14:27:35 +0530

    feat(buildroot): add configs and sources for am64x
    add configs and sources for buildroot distro for
    am64x evm platform.
    
    Signed-off-by: Mahammed Sadik Shaik <s-sadik@ti.com>

---
+++ configs/AM64X/AM64X_buildroot_tags.py
@@ -0,0 +1,6 @@
+# Project name and HTML title
+project = u'Buildroot for AM64x'
+html_title = 'Buildroot AM64x Documentation'
+
+# The master toctree document.
+master_doc = 'devices/AM64X/buildroot/index'
